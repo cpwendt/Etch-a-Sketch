8c5fd6ddfcdc687e7efd188e6fb0b338011d2907
cpwendt <168306589+cpwendt@users.noreply.github.com>
2024-09-15 22:13:02 -0700

    col and row subbed for cell added onmouseover fn

--- script.js
+++ script.js
@@ -1,20 +1,42 @@
 const container = document.querySelector(".container");
 
+
 function makeGrid() {
     for (r = 1; r <= 16; r++) {
-        const row = document.createElement("div")
+        // const row = document.createElement("div")
         // row.className = "row"
-        container.appendChild(row)
+        // container.appendChild(row)
         // row.textContent = "r"+i
 
         for (c = 1; c <= 16; c++) {
-            const col = document.createElement("col")
+            const cell = document.createElement("cell")
             // col.className = "col"
-            row.appendChild(col)
-            col.textContent = r + "," + c
-
+            container.appendChild(cell)
+            cell.textContent = r + "," + c
+            cell.onmouseover = function mouseOver() {
+                this.style.backgroundColor = "transparent"
+            }
         }
     }
+    // const cols = document.getElementsByClassName("col")
+    // for (cell = 1; cell <= 256; cell++) {
+    //     cols[cell].onmouseover = function mouseOver() {
+    //         this.style.backgroundColor = "red"
+    //     }
+    // }
 }
 
 makeGrid();
+console.log(document)
+
+
+
+// document.getElementsByClassName("col").onmouseover = function () { mouseOver() };
+
+// function mouseOver() {
+//     document.getElementById("col").style.backgroundColor = "red";
+// }
+
+// container.onmouseover = function mouseOver() {
+//     this.style.backgroundColor = "purple"
+// }
